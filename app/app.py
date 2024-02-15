67d76ae1b258fc2406f0ec0e841288473d98b5f0
Naeem Raza <naee.raza@zeropoint.hr>
2024-02-15 15:21:22 +0500

    Assignment 4

---
+++ app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import unittest
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World! This is the CICD using GitHub Actions" #add the line
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.tester = app.test_client(self)
+
+    def test_hello(self):
+        response = self.tester.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello, World!')
+
+if __name__ == '__main__':
+    # If the script is run directly, start the Flask app
+    app.run(debug=True, host='0.0.0.0', port=5000)
+else:
+    # If the script is imported, run the tests
+    unittest.main()
